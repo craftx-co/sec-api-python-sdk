d9ea6f9c36ff5d6a1db4f3a15dd29d4ffa6c3c8d
Parn Boonyamanee <parnpresso@gmail.com>
2020-04-04 15:49:58 +0700

    Add require package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sec-python-sdk",
-    version="1.0.0",
+    version="1.0.2",
     author="Parn Boonyamanee",
     author_email="parnpresso@gmail.com",
     description="Python SDK for access SEC data",
@@ -19,4 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+        "requests"
+    ],
 )
